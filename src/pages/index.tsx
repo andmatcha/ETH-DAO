097cbd6b0d5ffff7334bb11a290c3ebf4bfdb476
andmatcha <59753159+andmatcha@users.noreply.github.com>
2025-08-26 11:58:51 +0900

    section-1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,55 +1,43 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
+import { Sepolia } from "@thirdweb-dev/chains";
+import {
+  ConnectWallet,
+  useAddress,
+  useChain,
+  useContract,
+} from "@thirdweb-dev/react";
 import type { NextPage } from "next";
+
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="http://thirdweb.com/">thirdweb</a>!
-        </h1>
+  const address = useAddress();
+  const chain = useChain();
 
-        <p className={styles.description}>
-          Get started by configuring your desired network in{" "}
-          <code className={styles.code}>pages/_app.tsx</code>, then modify the{" "}
-          <code className={styles.code}>pages/index.tsx</code> file!
-        </p>
-
-        <div className={styles.connect}>
-          <ConnectWallet />
-        </div>
-
-        <div className={styles.grid}>
-          <a href="https://portal.thirdweb.com/" className={styles.card}>
-            <h2>Portal &rarr;</h2>
-            <p>
-              Guides, references and resources that will help you build with
-              thirdweb.
-            </p>
-          </a>
-
-          <a href="https://thirdweb.com/dashboard" className={styles.card}>
-            <h2>Dashboard &rarr;</h2>
-            <p>
-              Deploy, configure and manage your smart contracts from the
-              dashboard.
-            </p>
-          </a>
-
-          <a
-            href="https://portal.thirdweb.com/templates"
-            className={styles.card}
-          >
-            <h2>Templates &rarr;</h2>
-            <p>
-              Discover and clone template projects showcasing thirdweb features.
-            </p>
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+  // テストネットが Sepolia ではなかった場合に警告を表示
+  if (chain && chain.chainId !== Sepolia.chainId) {
+    console.log("wallet address: ", address);
+    console.log("chain name: ", chain.name);
+    return (
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Sepolia に切り替えてください⚠️</h1>
+          <p>この dApp は Sepolia テストネットのみで動作します。</p>
+          <p>ウォレットから接続中のネットワークを切り替えてください。</p>
+        </main>
+      </div>
+    );
+  } else {
+    return (
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Welcome to My First DAO !!</h1>
+          <div className={styles.connect}>
+            <ConnectWallet />
+          </div>
+        </main>
+      </div>
+    );
+  }
 };
 
 export default Home;
